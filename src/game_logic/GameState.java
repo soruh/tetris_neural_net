3a06fed1025dd3940abce8fde96b4f8c1e5c6f58
lebega <55129174+lebega@users.noreply.github.com>
2019-09-10 10:53:10 +0200

    blablabla

--- src/game_logic/GameState.java
+++ src/game_logic/GameState.java
@@ -1,18 +1,18 @@
-package Tetris;
-
 import java.util.Random;
 
 public class GameState {
 
     private int[][] screen, nextScreen;
     private int currentBlock, nextBlock;
-    private long tick;
+    private long ticks;
 
-    Random r = new Random();
+    Random r;
 
-    public GameState(){
+    public GameState(long pRandomSeed){
         screen = new int[22][10];
-        tick = 0;
+        ticks = 0;
+
+        r = new Random(pRandomSeed);
     }
 
     public void tick(Action pAction){
@@ -36,7 +36,34 @@ public class GameState {
 
     }
 
-    private boolean checkActionValid(Action pAction){
+    public void update(Action pAction){
+        switch (pAction){
+            case NOTHING: break;
+            case LEFT: this.moveLeft(); break;
+            case RIGHT: this.moveRight(); break;
+            case DOWN: this. moveDown(); break;
+            case TURN_LEFT: this.turnLeft(); break;
+            case TURN_RIGHT: this.turnRight(); break;
+        }
+    }
+
+    private void moveLeft(){
+
+    }
+
+    private void moveRight(){
+
+    }
+
+    private void moveDown(){
+
+    }
+
+    private void turnLeft(){
+
+    }
+
+    private void turnRight(){
 
     }
 }
