6dacb4bee360db3b98ce95242a3b15579c00145b
lebega <55129174+lebega@users.noreply.github.com>
2019-09-10 11:22:03 +0200

    started to implement class Block

--- src/game_logic/Block.java
+++ src/game_logic/Block.java
@@ -1,19 +1,24 @@
 package game_logic;
 
 import java.awt.geom.Point2D;
+import java.util.Vector;
 
 public abstract class Block {
 
-    private Point2D[] cells;
+    private int[][] cells;
     private int type;
 
 
     private void moveLeft(){
-
+        for(int[] cell : cells){
+            cell[0]  --;
+        }
     }
 
     private void moveRight(){
-
+        for(int[] cell : cells){
+            cell[0]  ++;
+        }
     }
 
     private void moveDown(){
