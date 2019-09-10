e012ab207325bd28537c7a73da3cd4dba8f554e0
lebega <55129174+lebega@users.noreply.github.com>
2019-09-10 11:53:45 +0200

    -

--- src/game_logic/Block.java
+++ src/game_logic/Block.java
@@ -1,58 +1,11 @@
 package game_logic;
 
-public abstract class Block {
+public class Block {
+    private static final
 
-    private int[][] cells;
     private int type;
-
-    public abstract void turnLeft();
-
-    public abstract void turnRight();
-
-    private void moveLeft(){
-        for(int[] cell : cells){
-            cell[0] --;
-        }
-    }
-
-    private void moveRight(){
-        for(int[] cell : cells){
-            cell[0] ++;
-        }
-    }
-
-    private void moveDown(){
-        for(int[] cell : cells) {
-            cell[1] --;
-        }
-    }
+    private int rotation;
+    private int[] position;
 
 
-    public class lPiece{
-
-    }
-
-    public class LPieceInverted{
-
-    }
-
-    public class TPiece{
-
-    }
-
-    public class IPiece{
-
-    }
-
-    public class BlockPiece{
-
-    }
-
-    public class StepPiece{
-
-    }
-
-    public class StepPieceInverted{
-
-    }
 }
