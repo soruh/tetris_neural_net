3ef6f9c1390f46f0e56662aff35144dfce94855a
soruh <mail@soruh.de>
2019-09-11 09:22:48 +0200

    implemented parallelSort

--- src/network/GeneticTrainer.java
+++ src/network/GeneticTrainer.java
@@ -1,6 +1,7 @@
 package network;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
 
 public class GeneticTrainer {
@@ -25,14 +26,50 @@ public class GeneticTrainer {
         for (int i = 0; i < networks.length; i++) {
             fitness[i] = func.evaluate(networks[i]);
         }
-        parrallelSort(fitness, networks);
+
+        parallelSort(fitness, networks);
     }
 
 
-    private static void parrallelSort(double[] sortBy, Object[] sortAlong) {
-        parrallelSort(sortBy, sortAlong, 0, sortBy.length);
-    }
-    private static void parrallelSort(double[] sortBy, Object[] sortAlong, int from, int length) {
+    private static void parallelSort(double[] sortBy, Object[] sortAlong) {
+        class Item {
+            double sortBy;
+            Object sortAlong;
 
+            Item (double sortBy, Object sortedValue) {
+                this.sortAlong = sortedValue;
+                this.sortBy = sortBy;
+            }
+
+            public double getSortBy() { return sortBy; }
+
+            public Object getSortAlong() { return sortAlong; }
+
+            public int compareTo(Item i2) {
+                if(this.sortBy == i2.sortBy) return 0;
+                if(this.sortBy > i2.sortBy) return 1;
+                return -1;
+            }
+        }
+
+        class ItemComparator implements Comparator {
+            public int compare( Object o1, Object o2 ) {
+                Item i1 = (Item)o1;
+                Item i2 = (Item)o2;
+
+                return i1.compareTo(i2);
+            }
+        }
+
+
+        Item[] items = new Item[sortBy.length];
+        for (int i = 0; i < sortBy.length; i++) items[i] = new Item(sortBy[i], sortAlong[i]);
+
+        Arrays.sort( items, new ItemComparator() );
+
+        for (int i = 0; i < sortBy.length; i++) {
+            sortBy[i] = items[i].getSortBy();
+            sortAlong[i] = items[i].getSortAlong();
+        }
     }
 }
