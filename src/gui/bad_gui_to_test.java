8de307032dc42429add283dd42ed02f82e6306c7
N3v3rm1nd002 <Justus.paul009@gmail.com>
2019-09-11 12:12:54 +0200

    "Fixed" GUI!?

--- src/gui/bad_gui_to_test.java
+++ src/gui/bad_gui_to_test.java
@@ -1,12 +1,16 @@
 package gui;
 
+import com.sun.webkit.ThemeClient;
 import game_logic.*;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.scene.canvas.*;
-import javafx.scene.text.*;
 import javafx.scene.paint.*;
+import javafx.event.*;
+import javafx.stage.WindowEvent;
 
 import java.util.Random;
 
@@ -16,95 +20,115 @@ public class bad_gui_to_test extends Application {
     private GraphicsContext gc;
     private Stage stage;
 
+    private boolean oddFrame = false;
+
     public static void main(String[] args) {
         Application.launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage pStage) throws Exception {
+        stage = pStage;
         stage.setTitle("Tetris AI");
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
 
         Group root = new Group();
         stage.setScene(new Scene(root));
 
-        Canvas canvas = new Canvas(500, 1000);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+        canvas = new Canvas(500, 1000);
+        gc = canvas.getGraphicsContext2D();
         root.getChildren().add(canvas);
 
         Random r = new Random();
         tetris = new Game(r.nextLong());
 
-        gc.setFill(Color.BLUE);
-        gc.fillRect(5, 5, 40, 40);
+        //gc.setFill(Color.BLUE);
+        //gc.fillRect(5, 5, 40, 40);
+
+        new AnimationTimer() {
+            public void handle(long now) {
+                if (oddFrame) {
+                    runGame();
+                }
+                oddFrame = !oddFrame;
+            }
+        }.start();
 
         stage.show();
+    }
 
-        //while (true) {
-            //tetris.tick(Action.NOTHING);
-            //GameState state = tetris.getGameState();
+    public void runGame() {
+        if (!tetris.tick(Action.DOWN)) {
+            System.out.println("Your final score is: " + tetris.getGameState().getScore());
+            Platform.exit();
+            System.exit(0);
+            return;
+        }
+        GameState state = tetris.getGameState();
 
-            //System.out.println(state.getScore());
+        System.out.println(state.getScore());
 
-            //int[][] grid = state.getGrid();
+        int[][] grid = state.getGrid();
 
-            for (int x = 0; x < 10; x++) {
-                for (int y = 0; y < 20; y++) {
-                    int color = 0;
-                    /*if (grid[y][x] < 0) {
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 20; y++) {
+                int color = 0;
+                if (grid[y][x] == 0) {
+                    int[][] blockSpots = state.getCurrentBlock().getAbsoluteCells();
+                    boolean onBlock = false;
+                    for (int[] xy : blockSpots) {
+                        if (y == xy[0] && x == xy[1]) {
+                            onBlock = true;
+                        }
+                    }
+                    if (onBlock) {
                         color = state.getCurrentBlock().getType();
                     } else {
-                        color = grid[x][y];
-                    }*/
-
-
-                    // DEBUGGING !!!!!!!!!!
-
-                    color = r.nextInt(8);
-
-                    // DEBUGGING !!!!!!!!!!!!!
-
-
-                    int brightness = 110;
-                    switch (color) {
-                        case 0:
-                            gc.setFill(Color.rgb(240, 240, 240));
-                            break;
-                        case 1:
-                            gc.setFill(Color.rgb(0, 2*brightness, 2*brightness));
-                            break;
-                        case 2:
-                            gc.setFill(Color.rgb(2*brightness, 2*brightness, 0));
-                            break;
-                        case 3:
-                            gc.setFill(Color.rgb(2*brightness, 0, 2*brightness));
-                            break;
-                        case 4:
-                            gc.setFill(Color.rgb(0, 2*brightness, 0));
-                            break;
-                        case 5:
-                            gc.setFill(Color.rgb(2*brightness, 0, 0));
-                            break;
-                        case 6:
-                            gc.setFill(Color.rgb(0, 0, 2*brightness));
-                            break;
-                        case 7:
-                            gc.setFill(Color.rgb(2*brightness, brightness, 0));
-                            break;
-
+                        color = 0;
                     }
-                    gc.fillRect(2 + x * 50, 952 - (y * 50), 46, 46);
+                } else {
+                    color = grid[y][x];
                 }
-                stage.show();
-                try {
-                    Thread.sleep(33);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+                int brightness = 110;
+                switch (color) {
+                    case 0:
+                        gc.setFill(Color.rgb(240, 240, 240));
+                        break;
+                    case 1:
+                        gc.setFill(Color.rgb(0, 2 * brightness, 2 * brightness));
+                        break;
+                    case 2:
+                        gc.setFill(Color.rgb(2 * brightness, 2 * brightness, 0));
+                        break;
+                    case 3:
+                        gc.setFill(Color.rgb(2 * brightness, 0, 2 * brightness));
+                        break;
+                    case 4:
+                        gc.setFill(Color.rgb(0, 2 * brightness, 0));
+                        break;
+                    case 5:
+                        gc.setFill(Color.rgb(2 * brightness, 0, 0));
+                        break;
+                    case 6:
+                        gc.setFill(Color.rgb(0, 0, 2 * brightness));
+                        break;
+                    case 7:
+                        gc.setFill(Color.rgb(2 * brightness, brightness, 0));
+                        break;
+
                 }
+                gc.fillRect(2 + x * 50, 952 - (y * 50), 46, 46);
             }
-        //}
+
+            stage.show();
+        }
     }
 
-    private void run() {
-
-    }
 }
